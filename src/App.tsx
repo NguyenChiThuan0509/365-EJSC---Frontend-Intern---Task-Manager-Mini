43f092dc381ee4f9c8403fa654ada0d3e8a29eae
NguyenChiThuan0509 <ncthuan0509@gmail.com>
2026-01-02 20:35:40 +0700

    change color background

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
-
 function App() {
   return (
     <div className="container">
       <div className="header">
-        <h1>Task Manager Mini</h1>
+        <div>
+          <h1>Task Manager Mini</h1>
+        </div>
         <div className="btn">
           <a href="!#">+ New Task</a>
         </div>
       </div>
-      {/* <div className="status">No status filter</div> */}
+      <div className="status">No status filter</div>
     </div>
   );
 }
